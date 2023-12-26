9d01ac489b029e722bad9a9fe8fdcdb1621e93e0
SoumyaCO <101652501+SoumyaCO@users.noreply.github.com>
2023-12-27 01:00:51 +0530

    added some extra route

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ app.get('/news', (req, res) =>{
     res.send("")
 })
 
+app.get('/dhamaka', (req, res) =>{
+    res.send("")
+})
+
+app.get('/tiu', (req, res) =>{
+    res.send("")
+})
+
+app.get('/rajPandey', (req, res) =>{
+    res.send("")
+});
+
 app.listen(5500, () => {
     console.log("http://localhost:5500")
 });
